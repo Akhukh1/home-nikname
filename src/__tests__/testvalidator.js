942cb70c364cacd342616e333b02a35f155f2355
Aleks Khukh <aleks.khukh@gmail.com>
2023-09-07 09:22:33 +0300

    первое задание

--- src/__tests__/testvalidator.js
+++ src/__tests__/testvalidator.js
@@ -3,43 +3,43 @@ import Validator from '../validator';
 test('test Validator letters begin "-"', () => {
   const userName = new Validator();
   const result = userName.validateUsername('-kino');
-  expect(result).toEqual('Имя пользователя не соответсвует правилам!');
+  expect(result).toEqual('Имя пользователя не соответствует правилам!');
 });
 
 test('test Validator letters begin "_"', () => {
   const userName = new Validator();
   const result = userName.validateUsername('_kino');
-  expect(result).toEqual('Имя пользователя не соответсвует правилам!');
+  expect(result).toEqual('Имя пользователя не соответствует правилам!');
 });
 
 test('test Validator letters begin "0-9"', () => {
   const userName = new Validator();
   const result = userName.validateUsername('5kino');
-  expect(result).toEqual('Имя пользователя не соответсвует правилам!');
+  expect(result).toEqual('Имя пользователя не соответствует правилам!');
 });
 
 test('test Validator letters end "-"', () => {
   const userName = new Validator();
   const result = userName.validateUsername('kino-');
-  expect(result).toEqual('Имя пользователя не соответсвует правилам!');
+  expect(result).toEqual('Имя пользователя не соответствует правилам!');
 });
 
 test('test Validator letters end "_"', () => {
   const userName = new Validator();
   const result = userName.validateUsername('kino_');
-  expect(result).toEqual('Имя пользователя не соответсвует правилам!');
+  expect(result).toEqual('Имя пользователя не соответствует правилам!');
 });
 
 test('test Validator letters end "0-9"', () => {
   const userName = new Validator();
   const result = userName.validateUsername('kino6');
-  expect(result).toEqual('Имя пользователя не соответсвует правилам!');
+  expect(result).toEqual('Имя пользователя не соответствует правилам!');
 });
 
 test('test Validator letters 3 "0-9"', () => {
   const userName = new Validator();
   const result = userName.validateUsername('kino6544sasha');
-  expect(result).toEqual('Имя пользователя не соответсвует правилам!');
+  expect(result).toEqual('Имя пользователя не соответствует правилам!');
 });
 
 test('test Validator letters Right', () => {
--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,11 @@
 export default class Validator {
   validateUsername(name) {
     let userName = '';
-    const validateName = /^(?![0-9\_\-])((([0-9]{0,3}[a-zA-Z\_\-]{1})|([a-zA-Z\_\-]{0,}))+)(?<![0-9\_\-])$/;
+    const validateName = /^(?![0-9_-])((([0-9]{0,3}[a-zA-Z_-]{1})|([a-zA-Z_-]{0,}))+)(?<![0-9_-])$/;
     if (validateName.test(name)) {
       userName = `Имя пользователя - ${name}`;
     } else {
-      userName = 'Имя пользователя не соответсвует правилам!';
+      userName = 'Имя пользователя не соответствует правилам!';
     }
     return userName;
   }
